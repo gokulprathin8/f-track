c2ccc0a74b0fa0147a7146e2feaccbcee099f359
Gokul Prathin A <gokulprathin8@gmail.com>
2023-05-17 04:44:43 -0400

    init+misc

---
+++ main.py
@@ -0,0 +1,188 @@
+from datetime import datetime
+from enum import Enum
+
+from beanie import Document, init_beanie, Link
+from bson import ObjectId
+from fastapi import FastAPI, HTTPException
+from motor.motor_asyncio import AsyncIOMotorClient
+from pydantic import BaseModel
+from starlette.requests import Request
+from starlette.responses import HTMLResponse
+from starlette.templating import Jinja2Templates
+
+app = FastAPI()
+
+templates = Jinja2Templates(directory="templates")
+
+
+class CategoryType(int, Enum):
+    income = 0
+    expenditure = 1
+
+
+class AccountType(int, Enum):
+    checking = 0
+    saving = 1
+    credit = 2
+
+
+class Categories(Document):
+    name: str
+    cat_type: CategoryType
+
+
+class Accounts(Document):
+    name: str
+    acc_type: AccountType
+    balance: int
+
+
+class Transactions(Document):
+    trans_date: datetime
+    category: Link[Categories]
+    description: str
+    account: Link[Accounts]
+    trans_type: AccountType
+    amount: int
+
+
+class TransactionsIn(BaseModel):
+    trans_date: datetime
+    category: Categories
+    description: str
+    account: Accounts
+    trans_type: AccountType
+    amount: int
+
+
+class CreateCategoryIn(BaseModel):
+    name: str
+    cat_type: CategoryType
+
+
+class AccountsIn(BaseModel):
+    name: str
+    acc_type: AccountType
+    balance: int
+
+
+@app.post("/create_category")
+async def create_category(category: CreateCategoryIn):
+    new_cat = Categories(
+        name=category.name,
+        cat_type=category.cat_type
+    )
+    await new_cat.insert()
+
+
+@app.put("/update_category/{category_id}")
+async def update_category(category_id: str, category: CreateCategoryIn):
+    # Fetch the existing category
+    existing_cat = await Categories.get_or_none(id=ObjectId(category_id))
+    if not existing_cat:
+        raise HTTPException(status_code=404, detail="Category not found")
+
+    # Update the category fields
+    existing_cat.name = category.name
+    existing_cat.cat_type = category.cat_type
+    await existing_cat.replace()
+    return {"message": "Category updated successfully"}
+
+
+@app.delete("/delete_category/{category_id}")
+async def delete_category(category_id: str):
+    # Fetch the existing category
+    existing_cat = await Categories.get_or_none(id=ObjectId(category_id))
+    if not existing_cat:
+        raise HTTPException(status_code=404, detail="Category not found")
+
+    # Delete the category
+    await existing_cat.delete()
+    return {"message": "Category deleted successfully"}
+
+
+@app.post("/create_transaction")
+async def create_transaction(trans: TransactionsIn):
+    new_trans = Transactions(
+        trans_date=trans.trans_date,
+        category=trans.category.id,
+        description=trans.description,
+        account=trans.account.id,
+        trans_type=trans.trans_type,
+        amount=trans.amount
+    )
+    await new_trans.insert()
+    return {"message": "Transaction created successfully"}
+
+
+@app.put("/update_transaction/{trans_id}")
+async def update_transaction(trans_id: str, trans: TransactionsIn):
+    existing_trans = await Transactions.get_or_none(id=ObjectId(trans_id))
+    if not existing_trans:
+        raise HTTPException(status_code=404, detail="Transaction not found")
+    existing_trans.trans_date = trans.trans_date
+    existing_trans.category = trans.category.id
+    existing_trans.description = trans.description
+    existing_trans.account = trans.account.id
+    existing_trans.trans_type = trans.trans_type
+    existing_trans.amount = trans.amount
+    await existing_trans.replace()
+    return {"message": "Transaction updated successfully"}
+
+
+@app.delete("/delete_transaction/{trans_id}")
+async def delete_transaction(trans_id: str):
+    existing_trans = await Transactions.get_or_none(id=ObjectId(trans_id))
+    if not existing_trans:
+        raise HTTPException(status_code=404, detail="Transaction not found")
+    await existing_trans.delete()
+    return {"message": "Transaction deleted successfully"}
+
+
+@app.post("/create_account")
+async def create_account(account: AccountsIn):
+    new_account = Accounts(
+        name=account.name,
+        acc_type=account.acc_type,
+        balance=account.balance
+    )
+    await new_account.insert()
+    return {"message": "Account created successfully"}
+
+
+@app.put("/update_account/{account_id}")
+async def update_account(account_id: str, account: Accounts):
+    # Fetch the existing account
+    existing_account = await Accounts.get_or_none(id=ObjectId(account_id))
+    if not existing_account:
+        raise HTTPException(status_code=404, detail="Account not found")
+
+    # Update the account fields
+    existing_account.name = account.name
+    existing_account.acc_type = account.acc_type
+    existing_account.balance = account.balance
+    await existing_account.replace()
+    return {"message": "Account updated successfully"}
+
+
+@app.delete("/delete_account/{account_id}")
+async def delete_account(account_id: str):
+    # Fetch the existing account
+    existing_account = await Accounts.get_or_none(id=ObjectId(account_id))
+    if not existing_account:
+        raise HTTPException(status_code=404, detail="Account not found")
+
+    # Delete the account
+    await existing_account.delete()
+    return {"message": "Account deleted successfully"}
+
+
+@app.on_event("startup")
+async def handle_startup():
+    client = AsyncIOMotorClient("mongodb://localhost:27017")
+    await init_beanie(database=client.db_name, document_models=[Categories, Transactions, Accounts])
+
+
+@app.get("/home", response_class=HTMLResponse)
+async def homepage(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
